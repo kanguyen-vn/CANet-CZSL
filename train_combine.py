d95fd465ebcd1cd63b23ca0753f03c76e572d61e
Kiet Nguyen <kanguyen.vn@gmail.com>
2023-12-04 23:30:07 -0500

    Debug combine dataset

--- train_combine.py
+++ train_combine.py
@@ -87,12 +87,10 @@ def generate_combined_dataset(
     combined_images_dir.mkdir(parents=True, exist_ok=True)
 
     for img_path in img_paths_to_copy:
-        print(f"{combined_images_dir = }")
-        print(f"{img_path = }")
-        print(f"{img_path.relative_to(img_path.parents[1]) = }")
         symlink_path = combined_images_dir / img_path.relative_to(img_path.parents[1])
         print(f"{symlink_path = }")
-        print(f"{symlink_path.resolve() = }")
+        print(f"{symlink_path.parent = }")
+        print(f"{(data_root / img_path).resolve() = }")
         symlink_path.parent.mkdir(parents=True, exist_ok=True)
         if not symlink_path.is_symlink():
             symlink_path.symlink_to((data_root / img_path).resolve())
